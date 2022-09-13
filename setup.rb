e645cbf70683118de884329fc937bda2bb8ebc2c
JC <blondel.jc@gmail.com>
2022-09-13 23:58:45 +0200

    Remove unused classes from earlier statments
    The responsabilities covered by those classes
    are packed in 'ChunkWorker' classes
    and it seems not necessary to use objects instantiations
    for now
    
    We would need to dispatch business logic inside smaller parts of code
    for a dedicated purpose
    ex from JobsChunkWorker:
      'category = store.professions[job[:profession_id]] || "Autre"'
    
      we probably want to report the unmatched profession_ids
      from another service
    
    Lead to discussions !

--- setup.rb
+++ setup.rb
@@ -18,13 +18,9 @@ require 'geocoding/continents_as_polygons'
 
 require 'processors/professions_processor'
 require 'processors/professions/professions_chunk_worker'
-require 'processors/professions/jobs_by_continent_builder'
 
 require 'processors/jobs_processor'
 require 'processors/jobs/jobs_chunk_worker'
-require 'processors/jobs/job_geocoder'
-require 'processors/jobs/category_matcher'
-require 'processors/jobs/jobs_by_continent_incrementor'
 
 require 'outputs/formatter'
 require 'outputs/printer'
